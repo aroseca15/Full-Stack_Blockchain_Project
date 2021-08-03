eed383a17049b5fc6974277b1b0067b5c386ec87
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2021-08-02 19:54:48 -0700

    created the cryptoHash function.

--- CryptoHash.js
+++ CryptoHash.js
@@ -1,5 +1,11 @@
+const crypto = require('crypto');
+
 const CryptoHash = (...inputs) =>{
-    
+const hash = crypto.createHash('sha256');
+
+hash.update(inputs.sort().join(' '));
+
+return hash.digest('hex');
 };
 
 module.exports = CryptoHash;
