9dbe937f85ce06a8c23320d6d1a67e5dc7b3d708
Your Name <aroseca15@gmail.com>
2021-08-03 18:25:02 -0700

    created basic blockchain class. All current tests pass.

--- Blockchain/Blockchain.js
+++ Blockchain/Blockchain.js
@@ -1,7 +1,16 @@
-
+const Block = require('../Block/Block');
 class Blockchain {
     constructor() {
+        this.chain = [Block.genesis()];
+    }
 
+    addBlock({ data }) {
+        const newBlock = Block.mineBlock({
+            lastBlock: this.chain[this.chain.length - 1],
+            data
+        });
+
+        this.chain.push(newBlock);
     }
 }
 
